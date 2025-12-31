5fac848537566fe4c286b8080356ea55337849c8
regan taigel <regantaigel@gmail.com>
2026-01-01 11:07:26 +1300

    Add project files.

---
+++ Haulory.Application/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace Haulory.Application
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ Haulory.Application/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace Haulory.Application
+{
+    // All the code in this file is only included on iOS.
+    public class PlatformClass1
+    {
+    }
+}
